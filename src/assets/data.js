18f64611be16eec87ff1129de647be31dd106fd3
yopiangga <yopigambyok@gmail.com>
2023-11-28 10:27:55 +0700

    Setup data

---
+++ src/assets/data.js
@@ -0,0 +1,357 @@
+export const data = [
+  {
+    id: "a1_1",
+    name: "RSUA",
+    images: ["a1_1_1.jpg", "a1_1_2.jpg"],
+  },
+  {
+    id: "a1_2",
+    name: "AGT/Store",
+    images: ["a1_2_1.jpg", "a1_2_2.jpg"],
+  },
+  {
+    id: "a1_3",
+    name: "AHCP",
+    images: ["a1_3_1.jpg", "a1_3_2.jpg"],
+  },
+  {
+    id: "a1_4",
+    name: "RSHP",
+    images: ["a1_4_1.jpg", "a1_4_2.jpg"],
+  },
+  {
+    id: "a2_1",
+    name: "BPBRIN Booth",
+    images: ["a2_1_1.jpg", "a2_1_2.jpg"],
+  },
+  {
+    id: "a3_1",
+    name: "Matching Fund",
+    images: ["a3_1_1.jpg", "a3_1_2.jpg"],
+  },
+  {
+    id: "a3_2",
+    name: "STEMP CELL (Prualure & GFS Scret)",
+    images: ["a3_2_1.jpg", "a3_2_2.jpg"],
+  },
+  {
+    id: "a3_3",
+    name: "Matching Fund",
+    images: ["a3_3_1.jpg", "a3_3_2.jpg"],
+  },
+  {
+    id: "a3_4",
+    name: "ULP Farmasai",
+    images: ["a3_4_1.jpg", "a3_4_2.jpg"],
+  },
+  {
+    id: "a4_1",
+    name: "FF",
+    images: ["a4_1_1.jpg", "a4_1_2.jpg"],
+  },
+  {
+    id: "a4_2",
+    name: "FK",
+    images: ["a4_2_1.jpg", "a4_2_2.jpg"],
+  },
+  {
+    id: "a4_3",
+    name: "FKP",
+    images: ["a4_3_1.jpg", "a4_3_2.jpg"],
+  },
+  {
+    id: "a4_4",
+    name: "FKG",
+    images: ["a4_4_1.jpg", "a4_4_2.jpg"],
+  },
+  {
+    id: "a5_1",
+    name: "FPK",
+    images: ["a5_1_1.jpg", "a5_1_2.jpg"],
+  },
+  {
+    id: "a5_2",
+    name: "FTMM",
+    images: ["a5_2_1.jpg", "a5_2_2.jpg"],
+  },
+  {
+    id: "a5_3",
+    name: "FIB-FKM (Likur Production)",
+    images: ["a5_3_1.jpg", "a5_3_2.jpg"],
+  },
+  {
+    id: "a5_4",
+    name: "FISIP (Board Game Arempaja",
+    images: ["a5_4_1.jpg", "a5_4_2.jpg"],
+  },
+  {
+    id: "a6_1",
+    name: "FH (ALC)",
+    images: ["a6_1_1.jpg", "a6_1_2.jpg"],
+  },
+  {
+    id: "a6_2",
+    name: "FEB (Zia Parfum)",
+    images: ["a6_2_1.jpg", "a6_2_2.jpg"],
+  },
+  {
+    id: "a6_3",
+    name: "Vokasi",
+    images: ["a6_3_1.jpg", "a6_3_2.jpg"],
+  },
+  {
+    id: "a6_4",
+    name: "FPSI",
+    images: ["a6_4_1.jpg", "a6_4_2.jpg"],
+  },
+  {
+    id: "a7_1",
+    name: "PT Indo Folra",
+    images: ["a7_1_1.jpg", "a7_1_2.jpg"],
+  },
+  {
+    id: "a7_2",
+    name: "DPKKA",
+    images: ["a7_2_1.jpg", "a7_2_2.jpg"],
+  },
+  {
+    id: "a7_3",
+    name: "PPMB",
+    images: ["a7_3_1.jpg", "a7_3_2.jpg"],
+  },
+  {
+    id: "a7_4",
+    name: "Pusat Halal",
+    images: ["a7_4_1.jpg", "a7_4_2.jpg"],
+  },
+
+  {
+    id: "b1_1",
+    name: "ITS",
+    images: ["b1_1_1.jpg", "b1_1_2.jpg"],
+  },
+  {
+    id: "b1_2",
+    name: "UPN",
+    images: ["b1_2_1.jpg", "b1_2_2.jpg"],
+  },
+  {
+    id: "b1_3",
+    name: "UNM (DLI Edu Venture)",
+    images: ["b1_3_1.jpg", "b1_3_2.jpg"],
+  },
+  {
+    id: "b1_4",
+    name: "UNESA",
+    images: ["b1_4_1.jpg", "b1_4_2.jpg"],
+  },
+  {
+    id: "b2_1",
+    name: "PENS",
+    images: ["b2_1_1.jpg", "b2_1_2.jpg"],
+  },
+  {
+    id: "b2_2",
+    name: "PPNS (Inkubator Bisnis)",
+    images: ["b2_2_1.jpg", "b2_2_2.jpg"],
+  },
+  {
+    id: "b2_3",
+    name: "Heztek Coding",
+    images: ["b2_3_1.jpg", "b2_3_2.jpg"],
+  },
+  {
+    id: "b2_4",
+    name: "Artiarta",
+    images: ["b2_3_1.jpg", "b2_4_2.jpg"],
+  },
+
+  {
+    id: "b3_1",
+    name: "Braja",
+    images: ["b3_1_1.jpg", "b3_1_2.jpg"],
+  },
+  {
+    id: "b3_2",
+    name: "IBE Reality",
+    images: ["b3_2_1.jpg", "b3_2_2.jpg"],
+  },
+  {
+    id: "b3_3",
+    name: "Gelatah",
+    images: ["b3_3_1.jpg", "b3_3_2.jpg"],
+  },
+  {
+    id: "b3_4",
+    name: "Markas Walet",
+    images: ["b3_3_1.jpg", "b3_4_2.jpg"],
+  },
+
+  {
+    id: "c1_1",
+    name: "Bhagalen",
+    images: ["c1_1_1.jpg", "c1_1_2.jpg"],
+  },
+  {
+    id: "c1_2",
+    name: "Kakidiabet + Vascular Indonesia",
+    images: ["c1_2_1.jpg", "c1_2_2.jpg"],
+  },
+  {
+    id: "c1_3",
+    name: "Medical Tourism",
+    images: ["c1_3_1.jpg", "c1_3_2.jpg"],
+  },
+  {
+    id: "c1_4",
+    name: "Velife",
+    images: ["c1_4_1.jpg", "c1_4_2.jpg"],
+  },
+
+  {
+    id: "c2_1",
+    name: "Spriralife",
+    images: ["c2_1_1.jpg", "c2_1_2.jpg"],
+  },
+  {
+    id: "c2_2",
+    name: "Airlangga Agency",
+    images: ["c2_2_1.jpg", "c2_2_2.jpg"],
+  },
+  {
+    id: "c2_3",
+    name: "Digiteev",
+    images: ["c2_3_1.jpg", "c2_3_2.jpg"],
+  },
+  {
+    id: "c2_4",
+    name: "Semicolon",
+    images: ["c2_4_1.jpg", "c2_4_2.jpg"],
+  },
+
+  {
+    id: "c3_1",
+    name: "Dibi Bronie",
+    images: ["c3_1_1.jpg", "c3_1_2.jpg"],
+  },
+  {
+    id: "c3_2",
+    name: "Vettalk",
+    images: ["c3_2_1.jpg", "c3_2_2.jpg"],
+  },
+  {
+    id: "c3_3",
+    name: "Bayi Bunda",
+    images: ["c3_3_1.jpg", "c3_3_2.jpg"],
+  },
+  {
+    id: "c3_4",
+    name: "Teman Menyusui",
+    images: ["c3_4_1.jpg", "c3_4_2.jpg"],
+  },
+
+  {
+    id: "c4_1",
+    name: "Kanvas Tour",
+    images: ["c4_1_1.jpg", "c4_1_2.jpg"],
+  },
+  {
+    id: "c4_2",
+    name: "Goolive",
+    images: ["c4_2_1.jpg", "c4_2_2.jpg"],
+  },
+  {
+    id: "c4_3",
+    name: "Hidata",
+    images: ["c4_3_1.jpg", "c4_3_2.jpg"],
+  },
+  {
+    id: "c4_4",
+    name: "Jobhun",
+    images: ["c4_4_1.jpg", "c4_4_2.jpg"],
+  },
+
+  {
+    id: "d1_1",
+    name: "Maskopat",
+    images: ["d1_1_1.jpg", "d1_1_2.jpg"],
+  },
+  {
+    id: "d1_2",
+    name: "Kopi Celup Gahwi",
+    images: ["d1_2_1.jpg", "d1_2_2.jpg"],
+  },
+  {
+    id: "d1_3",
+    name: "Yorry Eatry",
+    images: ["d1_3_1.jpg", "d1_3_2.jpg"],
+  },
+  {
+    id: "d1_4",
+    name: "Yuksri",
+    images: ["d1_4_1.jpg", "d1_4_2.jpg"],
+  },
+
+  {
+    id: "d2_1",
+    name: "Shaany",
+    images: ["d2_1_1.jpg", "d2_1_2.jpg"],
+  },
+  {
+    id: "d2_2",
+    name: "Lomeal",
+    images: ["d2_2_1.jpg", "d2_2_2.jpg"],
+  },
+  {
+    id: "d2_3",
+    name: "Madu Onggu",
+    images: ["d2_3_1.jpg", "d2_3_2.jpg"],
+  },
+  {
+    id: "d2_4",
+    name: "Haust (Second Please)",
+    images: ["d2_4_1.jpg", "d2_4_2.jpg"],
+  },
+
+  {
+    id: "d3_1",
+    name: "Calmantea",
+    images: ["d3_1_1.jpg", "d3_1_2.jpg"],
+  },
+  {
+    id: "d3_2",
+    name: "Botega",
+    images: ["d3_2_1.jpg", "d3_2_2.jpg"],
+  },
+  {
+    id: "d3_3",
+    name: "Castuarine",
+    images: ["d3_3_1.jpg", "d3_3_2.jpg"],
+  },
+  {
+    id: "d3_4",
+    name: "Gyarus.id",
+    images: ["d3_4_1.jpg", "d3_4_2.jpg"],
+  },
+
+  {
+    id: "d4_1",
+    name: "Konveto Clothing Green",
+    images: ["d4_1_1.jpg", "d4_1_2.jpg"],
+  },
+  {
+    id: "d4_2",
+    name: "SiReduksi",
+    images: ["d4_2_1.jpg", "d4_2_2.jpg"],
+  },
+  {
+    id: "d4_3",
+    name: "Vetpicturean",
+    images: ["d4_3_1.jpg", "d4_3_2.jpg"],
+  },
+  {
+    id: "d4_4",
+    name: "Batik Warujuang",
+    images: ["d4_4_1.jpg", "d4_4_2.jpg"],
+  },
+];
